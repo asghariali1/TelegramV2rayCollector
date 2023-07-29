d2dac9edef7fb4bd98abc561e31586db30a1bec4
asghariali1 <77733571+asghariali1@users.noreply.github.com>
2023-07-29 15:58:17 +0330

    Update collect2send.php

--- collect2send.php
+++ collect2send.php
@@ -1,34 +1,8 @@
 <?php
 
-function create_url()
-{
-    $data = json_decode(file_get_contents("https://raw.githubusercontent.com/asghariali1/TelegramV2rayCollector/main/sub/mix_base64"), true);
-    $url = "";
-    foreach ($data as $key => $value) {
-        if ($key === "worker") {
-            $url = "https://" . $value;
-        } elseif ($key === "sub") {
-            if ($value !== "") {
-                $url .= "/sub/" . $value;
-            } else {
-                $url .= "/sub";
-            }
-        } else {
-            if ($value !== "") {
-                if (strpos($url, "?") !== false) {
-                    $url .= "&" . $key . "=" . $value;
-                } else {
-                    $url .= "?" . $key . "=" . $value;
-                }
-            }
-        }
-    }
-    return $url;
-}
-
 function save_sub()
 {
-    $url = create_url();
+    $url = 'https://raw.githubusercontent.com/asghariali1/TelegramV2rayCollector/main/sub/mix_base64';
     echo $url;
     file_put_contents('mix.txt', file_get_contents($url));
 }
