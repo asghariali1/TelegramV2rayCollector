9bc29267c68eefb7050f005d1e30fa44cb376a2a
asghariali1 <77733571+asghariali1@users.noreply.github.com>
2023-08-01 14:21:17 +0330

    Update collect2send.php

--- collect2send.php
+++ collect2send.php
@@ -2,7 +2,7 @@
 
 function save_sub()
 {
-    $url = 'https://raw.githubusercontent.com/asghariali1/TelegramV2rayCollector/main/sub/mix_base64';
+    $url = 'https://raw.githubusercontent.com/asghariali1/TelegramV2rayCollector/main/sub/mix_base64 ';
     echo $url;
     file_put_contents('mix.txt', file_get_contents($url));
 }
